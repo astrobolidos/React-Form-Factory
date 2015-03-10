0020b0541b64f13f2d984c8652339ec3ecaf05e8
astrobolidos <lisandroalavez@gmail.com>
2015-03-10 12:13:55 +0000

    update chtrlCheckbox to support material-ui

--- js/components/ctrlCheckbox.react.js
+++ js/components/ctrlCheckbox.react.js
@@ -1,13 +1,26 @@
 var React = require('react/addons');
+var MUI = require('material-ui');
+var Checkbox = MUI.Checkbox;
 
 var CtrlCheckbox = React.createClass({
 	render: function() {
 		return (
 			<p>
-				<label>{this.props.control.title}</label>
-				<input type="checkbox" name="ckbx" value="option 1"/> Value One
-				<input type="checkbox" name="ckbx" value="option 2"/> Value One
-				<input type="checkbox" name="ckbx" value="option 3"/> Value One
+				<h5>{this.props.control.title}</h5>
+				<Checkbox
+				  name="checkboxName1"
+				  value="checkboxValue1"
+				  label="went for a run today" />
+				<Checkbox
+				  name="checkboxName2"
+				  value="checkboxValue2"
+				  label="fed the dog"
+				  defaultChecked={true} />
+				<Checkbox
+				  name="checkboxName3"
+				  value="checkboxValue3"
+				  label="built a house on the moon"
+				  disabled={true} />
 			</p> 
 		);
 	}
