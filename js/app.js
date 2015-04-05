f81a6f2ef9df0b925ac5bc1e2f4b3c33cae69b11
astrobolidos <lisandroalavez@gmail.com>
2015-04-05 15:21:24 +0100

    added missing dependency
    calendar relies on tap-event plugin.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,15 @@ require('../css/styles.less');
 window.React = require('react/addons');
 
 var React = require('react/addons');
+var injectTapEventPlugin = require("react-tap-event-plugin");
+
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
+
+
 var QuestionList = require('./components/questionList.react');
 
 
